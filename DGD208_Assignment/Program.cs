197f97fb70884e8270ea9e631d712b5248316974
Zeynep Belinay Koçaker <zbkocaker@gmail.com>
2025-05-13 01:02:43 +0300

    umuyorum doğru yoldayımdır lol

--- DGD208_Assignment/Program.cs
+++ DGD208_Assignment/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace DGD208_Assignment
 {
-    internal class Program
+    internal class myLittleTamagotchi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("//meet with your pathfinder.");
+            Console.WriteLine(@"
+ █     █░ ▒█████   ███▄    █ ▓█████▄ ▓█████  ██▀███   ██▓    ▄▄▄       ███▄    █ ▓█████▄ 
+▓█░ █ ░█░▒██▒  ██▒ ██ ▀█   █ ▒██▀ ██▌▓█   ▀ ▓██ ▒ ██▒▓██▒   ▒████▄     ██ ▀█   █ ▒██▀ ██▌
+▒█░ █ ░█ ▒██░  ██▒▓██  ▀█ ██▒░██   █▌▒███   ▓██ ░▄█ ▒▒██░   ▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌
+░█░ █ ░█ ▒██   ██░▓██▒  ▐▌██▒░▓█▄   ▌▒▓█  ▄ ▒██▀▀█▄  ▒██░   ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌
+░░██▒██▓ ░ ████▓▒░▒██░   ▓██░░▒████▓ ░▒████▒░██▓ ▒██▒░██████▒▓█   ▓██▒▒██░   ▓██░░▒████▓ 
+░ ▓░▒ ▒  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░▓  ░▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒ 
+  ▒ ░ ░    ░ ▒ ▒░ ░ ░░   ░ ▒░ ░ ▒  ▒  ░ ░  ░  ░▒ ░ ▒░░ ░ ▒  ░ ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒ 
+  ░   ░  ░ ░ ░ ▒     ░   ░ ░  ░ ░  ░    ░     ░░   ░   ░ ░    ░   ▒      ░   ░ ░  ░ ░  ░ 
+    ░        ░ ░           ░    ░       ░  ░   ░         ░  ░     ░  ░         ░    ░    
+                              ░                                                   ░");
+
+            //pets vixen = new pets();
+            pets resa = new pets();
+            pets leza = new pets();
+
+            resa.Name = "Resa";
+            resa.Species = "the cunning vixen";
+            resa.Figure = @"
+      |\__/|
+     /     \
+    /_.~ ~,_\
+       \@/
+";
+
+            leza.Name = "Leza";
+            leza.Species = "the luna moth";
+            leza.Figure = @" 
+      __ \/ __
+     /o \{}/ o\
+     \   ()   /
+      `> /\ <`
+      (o/\/\o)
+       )    (
+";
+
+            resa.Meeting();
+            leza.Meeting();
+
+            //Console.WriteLine("now you can see me.");
+            //Console.WriteLine($"the cards gave me the name {Name}.");
+            Console.Read();
+
+
         }
     }
 }
---
+++ DGD208_Assignment/pets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGD208_Assignment
+{
+    internal class pets
+    {
+        public string Name;
+        public string Species;
+        public string Figure;
+
+
+        public void Meeting()
+        {
+            Console.WriteLine("Now you can see me.");
+            Console.WriteLine();
+            Console.WriteLine($"The cards gave me the name {Name}. I am {Species}.");
+            Console.WriteLine($"{Figure}");
+
+        }
+
+            
+
+
+    }
+}
